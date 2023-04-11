9198df2761517a7a2d744f0ecc01e3f0d9557567
shubh11me <todkarshubham11@gmail.com>
2023-04-11 21:31:15 +0530

    xaax

---
+++ header.inc.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="navbar.css">
+</head>
+
+<body>
+
+<header>  
+<nav>  
+<ul>  
+<li>  
+<a href="./index.php"> User </a>  
+</li> 
+<li>  
+<a href="./user_list.php"> User List</a>  
+</li>  
+
+<li>  
+<a href="teacher.php"> Teacher </a>  
+</li>  
+<li>  
+<a href="#"> Contact </a>  
+</li>  
+<li> <a href="#"> Terms of use </a>  
+</li>  
+<li>  
+<a href="#"> Join Us </a>  
+</li>  
+</ul>  
+</nav>  
+</header>  
--- user_list.php
+++ user_list.php
@@ -1,45 +1,7 @@
 <?php
 include('./conn.php');
+include('./header.inc.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Forms</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="navbar.css">
-</head>
-
-<body>
-
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <a href="./index.php"> User </a>
-                </li>
-
-                <li>
-                    <a href="./user_list.php"> User List</a>
-                </li>
-
-                <li>
-                    <a href="teacher.php"> Teacher </a>
-                </li>
-                <li>
-                    <a href="#"> Contact </a>
-                </li>
-                <li> <a href="#"> Terms of use </a>
-                </li>
-                <li>
-                    <a href="#"> Join Us </a>
-                </li>
-            </ul>
-        </nav>
-    </header>
 
     <div class="container">
         <table class="table table-hover">
@@ -119,7 +81,6 @@ include('./conn.php');
 
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-
-</html>
+    <?php
+include('./footer.inc.php');
+?>
